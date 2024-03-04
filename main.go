c1ca75df6ed7c80a75e4d0d3e24cea8301b4e00e
Meg <megclee88@gmail.com>
2024-03-04 00:48:05 -0800

    Improved Stat functions to determine the winner of simulated battles + added a function to properly capitalize Pokemon names + moved the random number generator to global scope

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"math/rand"
 	"strconv"
+	"strings"
 	"time"
 
 	pokeapi "github.com/mtslzr/pokeapi-go"
 	"github.com/mtslzr/pokeapi-go/structs"
 )
+
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func getStats(pokemonID int) (structs.Pokemon, error) {
 	pokemon, err := pokeapi.Pokemon(strconv.Itoa(pokemonID))
@@ -21,27 +24,57 @@ func getStats(pokemonID int) (structs.Pokemon, error) {
 
 }
 
+func sumBaseStats(pokemon structs.Pokemon) int {
+	total := 0
+
+	for _, stat := range pokemon.Stats {
+		total += stat.BaseStat
+	}
+
+	return total
+}
+
 func simulateBattle() {
 
-	rand.Seed(time.Now().UnixNano())
-	pokemon1ID := rand.Intn(151) + 1
-	pokemon2ID := rand.Intn(151) + 1
+	pokemon1ID := r.Intn(151) + 1
+	pokemon2ID := r.Intn(151) + 1
 
 	pokemon1, _ := getStats(pokemon1ID)
 	pokemon2, _ := getStats(pokemon2ID)
 
-	pokemon1Power := pokemon1.Stats[0].BaseStat + pokemon2.Stats[0].BaseStat
-	pokemon2Power := pokemon2.Stats[0].BaseStat + pokemon1.Stats[0].BaseStat
+	winner := determineWinner(pokemon1, pokemon2)
 
-	fmt.Println("Battle between", pokemon1.Name, "and", pokemon2.Name)
+	fmt.Println("Battle between", pokemon1ID, capitalizeName(pokemon1.Name), "and", pokemon2ID, capitalizeName(pokemon2.Name))
+	fmt.Println(winner, "wins!")
+}
+
+func determineWinner(pokemon1 structs.Pokemon, pokemon2 structs.Pokemon) string {
+	pokemon1Power := sumBaseStats(pokemon1)
+	pokemon2Power := sumBaseStats(pokemon2)
 
 	if pokemon1Power > pokemon2Power {
-		fmt.Println(pokemon1.Name, "wins!")
+		return capitalizeName(pokemon1.Name)
 	} else if pokemon2Power > pokemon1Power {
-		fmt.Println(pokemon2.Name, "wins!")
+		return capitalizeName(pokemon2.Name)
 	} else {
-		fmt.Println("It's a tie!")
+		return "It's a tie!"
 	}
+}
+
+// Function to capitalize the first letter of the Pokemon's names
+func capitalizeName(s string) string {
+
+	if len(s) == 0 {
+		return s
+	}
+	first := strings.ToUpper(string(s[0]))
+
+	if len(s) == 1 {
+		return first
+	}
+
+	rest := s[1:]
+	return first + rest
 }
 
 func main() {
